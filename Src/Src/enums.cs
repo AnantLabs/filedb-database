c1fc49373a7b19d3e10a486895ede02fe3bd99e7
brettg.perth@gmail.com <brettg.perth@gmail.com@9b13e5fc-624e-f87c-fcef-64a3843571ad>
2011-10-23 05:15:51 +0000

    added Enum to enums

--- Src/Src/enums.cs
+++ Src/Src/enums.cs
@@ -7,24 +7,24 @@ namespace FileDbNs
     /// <summary>
     /// Specifies the data type for database Fields
     /// </summary>
-    public enum DataType { String, Byte, Int, UInt, Float, Double, Bool, DateTime }
+    public enum DataTypeEnum { String, Byte, Int, UInt, Float, Double, Bool, DateTime }
 
     //=====================================================================
     /// <summary>
     /// Specifies the type of match for FilterExpressions with String data types
     /// </summary>
-    public enum MatchType { UseCase, IgnoreCase }
+    public enum MatchTypeEnum { UseCase, IgnoreCase }
 
     //=====================================================================
     /// <summary>
     /// Specifies the comparison operator to use for FilterExpressions
     /// </summary>
-    public enum Equality { Equal, NotEqual, GreaterThan, GreaterThanOrEqual, LessThan, LessThanOrEqual, In, NotIn, Like, NotLike }
+    public enum EqualityEnum { Equal, NotEqual, GreaterThan, GreaterThanOrEqual, LessThan, LessThanOrEqual, In, NotIn, Like, NotLike }
 
     //=====================================================================
     /// <summary>
     /// Boolean operands to use to join FilterExpressions 
     /// </summary>
-    public enum BoolOp { And, Or }
+    public enum BoolOpEnum { And, Or }
 
 }
