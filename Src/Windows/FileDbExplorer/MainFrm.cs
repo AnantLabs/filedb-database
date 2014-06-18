dfaa5986c9026bd568b00c945acdbb1e5b6987d1
brettg.perth@gmail.com <brettg.perth@gmail.com@9b13e5fc-624e-f87c-fcef-64a3843571ad>
2014-06-18 11:51:56 +0000

    fixed temp db copying fixed rename/delete field

--- Src/Windows/FileDbExplorer/MainFrm.cs
+++ Src/Windows/FileDbExplorer/MainFrm.cs
@@ -615,6 +615,7 @@ namespace FileDbExplorer
                     var vs = _rClickNode.Text.Split( '[' );
                     string text = string.Format( "{0} [{1}", dlg.Value, vs[1] );
                     _rClickNode.Text = text;
+                    nodeInfo.Tag = dlg.Value;
                     this.Cursor = Cursors.Default;
                 }
 
